276bb4f869d97f4126c36e4952e29ee6ae008c1f
Fabienne-Guerin <90317347+Fabienne-Guerin@users.noreply.github.com>
2022-07-21 22:17:21 +0200

    Revert "Front"

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { ItemController, CategoryController, ProductController } = require("./controllers");
+const { ItemController } = require("./controllers");
 
 const router = express.Router();
 
@@ -9,17 +9,5 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
-
-router.get("/categories", CategoryController.browse);
-router.get("/category/:id", CategoryController.read);
-router.put("/category/:id", CategoryController.edit);
-router.post("/category", CategoryController.add);
-router.delete("/category/:id", CategoryController.delete);
-
-router.get("/products", ProductController.browse);
-router.get("/product/:id", ProductController.read);
-router.put("/product/:id", ProductController.edit);
-router.post("/product", ProductController.add);
-router.delete("/product/:id", ProductController.delete);
 
 module.exports = router;
