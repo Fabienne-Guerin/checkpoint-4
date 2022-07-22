f95c07e76e7465206e24c9a0ef6d5b64b0212bf9
Fabienne-Guerin <fabienne.guerin]hotmail.fr>
2022-07-22 03:58:09 +0200

    add possible on admin form

--- frontend/src/pages/AdminProducts.jsx
+++ frontend/src/pages/AdminProducts.jsx
@@ -37,11 +37,11 @@ export default function AdminProducts() {
         </div>
       </div>
       <button
-          type="button"
-          className="p-2 bg-teal-900 min-w-1/3 lg:w-1/3 rounded-lg font-semibold mt-2 hover:bg-teal-500 text-white"
-        >
-          <Link to="/AdminForm">Ajouter un produit</Link>
-        </button>
+        type="button"
+        className="p-2 bg-teal-900 min-w-1/3 lg:w-1/3 rounded-lg font-semibold mt-2 hover:bg-teal-500 text-white"
+      >
+        <Link to="/AdminForm">Ajouter un produit</Link>
+      </button>
     </div>
   );
 }
